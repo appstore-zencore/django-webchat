29a3dd31640ad5e5a8037140a6f1e385e51c1aac
zencore <appstore@zencore.cn>
2018-05-11 17:54:54 +0800

    add doc

--- src/django_webchat/models.py
+++ src/django_webchat/models.py
@@ -3,19 +3,24 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth import get_user_model
 
 
-class Session(models.Model):
+class Talk(models.Model):
     sid = models.UUIDField(unique=True)
     name = models.CharField(max_length=16, null=True, blank=True)
+
+class P2pTalk(Talk):
+    pass
+
+class GroupTalk(Talk):
     owner = models.ForeignKey("Member", on_delete=models.CASCADE, null=True, blank=True, related_name="messages")
     announcement = models.CharField(max_length=64, null=True, blank=True)
 
 class Member(models.Model):
-    session = models.ForeignKey(Session, on_delete=models.CASCADE, related_name="members")
+    session = models.ForeignKey(Talk, on_delete=models.CASCADE, related_name="members")
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="members")
     name = models.CharField(max_length=16, null=True, blank=True)
 
 class Message(models.Model):
-    session = models.ForeignKey(Session, on_delete=models.CASCADE, related_name="messages")
+    session = models.ForeignKey(Talk, on_delete=models.CASCADE, related_name="messages")
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="messages")
     time = models.DateTimeField()
     content = models.TextField()
