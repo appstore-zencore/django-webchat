8a3d8f9af5b99480814c6e51113801d7956fb640
zencore <appstore@zencore.cn>
2018-05-11 12:51:34 +0800

    add django_webchat app

---
+++ src/django_webchat/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoWebchatConfig(AppConfig):
+    name = 'django_webchat'
---
+++ src/django_webchat/settings.py
@@ -0,0 +1,4 @@
+from django.conf import settings
+
+
+REGISTER_DJANGO_WEBCHAT_ADMIN = getattr(settings, "REGISTER_DJANGO_WEBCHAT_ADMIN", False)
